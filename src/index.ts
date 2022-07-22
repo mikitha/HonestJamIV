3579fd9c82ec4de07287078129bd5e988f2765b5
Bill Rossi <bassguitarbill@gmail.com>
2022-07-22 19:58:34 -0400

    Add a canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 console.log("Hello world");
+
+let canvas: HTMLCanvasElement;
+window.addEventListener('load', async () => {
+  canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+  resizeCanvas();
+
+  canvas.getContext('2d')?.fillRect(200, 200, 200, 200);
+});
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resizeCanvas);
