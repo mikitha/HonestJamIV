999810f859c6c8bb17938df3ae1faea7c59c1972
Michelle Tharp <mikitharp@gmail.com>
2022-07-23 17:04:02 -0400

    Pump make progress bar go up

--- src/PresserWorkstation.ts
+++ src/PresserWorkstation.ts
@@ -4,6 +4,7 @@ import Game from './Game.js';
 export default class PresserWorkstation implements Workstation {
   clickableObjects = [];
   progress = 0;
+  fullProgress = 20;
   currentCrankPosition = 0;
   lastCrankPosition = 0;
   crankBoundaryTop = 100;
@@ -18,27 +19,56 @@ export default class PresserWorkstation implements Workstation {
     this.currentCrankPosition = Math.min(this.crankBoundaryBottom, this.currentCrankPosition)
     this.currentCrankPosition = Math.max(this.crankBoundaryTop, this.currentCrankPosition) 
 
+    if (this.progress < this.fullProgress){
+    
     if(this.crankBoundaryBottom == this.currentCrankPosition && this.crankMovingUp == false){
         this.progress += 1
-        console.log(this.progress)
+     
         this.crankMovingUp = true
     }
 
     if(this.crankBoundaryTop == this.currentCrankPosition && this.crankMovingUp == true){
       this.progress += 1
-        console.log(this.progress)
+       
         this.crankMovingUp = false
     }
+  
+    if(this.progress == this.fullProgress){
+
+    console.log("done!")
+    }
   }
+}
 
   draw(ctx: CanvasRenderingContext2D) {
     this.drawCrank(ctx);
+    this.drawProgress(ctx);
     ctx.beginPath();       // Start a new path
     ctx.moveTo(400, this.crankBoundaryTop);    // Move the pen to (30, 50)
     ctx.lineTo(600, this.crankBoundaryTop);  // Draw a line to (150, 100)
     ctx.moveTo(400, this.crankBoundaryBottom)
     ctx.lineTo(600, this.crankBoundaryBottom)
     ctx.stroke(); 
+  }
+
+  drawProgress(ctx: CanvasRenderingContext2D){
+    ctx.strokeRect(20, 600, 200, 25);
+
+    if(this.progress <= 5){
+      ctx.fillStyle = "red"
+    }
+   else if(this.progress <= 10){
+      ctx.fillStyle = "purple"
+    }
+    else if (this.progress <= 15){
+      ctx.fillStyle = "blue"
+    }
+    else if (this.progress <= 20){
+      ctx.fillStyle = "green"
+    }
+
+    ctx.fillRect(20, 600, this.progress*10, 25);
+
   }
 
   drawCrank(ctx: CanvasRenderingContext2D){
